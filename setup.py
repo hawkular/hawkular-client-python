78747b26f642af4d1404df5a3a6d08160f07d2f0
Michael Burman <yak@iki.fi>
2016-05-04 16:13:54 +0300

    Change version to 0.4.0, add classifiers and readme conversion from markdown to rSt

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from os import path
+from setuptools.command.install import install
+import pypandoc
 
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+# Create rst here from Markdown
+z = pypandoc.convert('README.md','rst',format='markdown')
+
+with open('README.rst','w') as outfile:
+    outfile.write(z)
+    
 setup(name='hawkular-client',
       version='0.4.0',
-      description='Python client to communicate with Hawkular over HTTP(S)',
+      description='Python client to communicate with Hawkular server over HTTP(S)',
       author='Michael Burman',
       author_email='miburman@redhat.com',
+      license='Apache License 2.0',
       url='http://github.com/hawkular/hawkular-client-python',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Monitoring',
+      ],
       packages=['hawkular']
       )
