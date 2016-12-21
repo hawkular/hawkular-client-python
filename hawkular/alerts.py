52e1edd5e0e3d8d9f2fbb5aac7fe0f245b0b6c58
dkorn <dkorn@redhat.com>
2016-12-21 11:09:39 +0200

    Add support for group trigger deletion in hawkular alerts
    Adding delete_group_trigger method in hawkular alerts and
    test_delete_group_trigger to alerts tests.
    
    Since the `DELETE` action returns only a response code, without body,
    I added `parse_json` flag to `_http` method in `hawkular/client.py`.
    Without this addition an error is raised when trying to load
    the json with no content. I'm setting it to `True` by default,
    while sending it with `False` from the `_delete` method.
    This Issue was solved in `hawkular/metrics.py` by checking that the
    method is `GET`, a wrong solution IMHO cause `POST` and `PUT` methods
    also return body.

--- hawkular/alerts.py
+++ hawkular/alerts.py
@@ -155,6 +155,16 @@ class HawkularAlertsClient(HawkularBaseClient):
         data = self._serialize_object(trigger)
         return Trigger(self._post(self._service_url(['triggers', 'groups']), data))
 
+    def delete_group_trigger(self, group_id, keep_non_orphans=False, keep_orphans=False):
+         """
+         Delete a group trigger
+         :param group_id: ID of the group trigger to delete
+         :param keep_non_orphans: if True converts the non-orphan member triggers to standard triggers
+         :param keep_orphans: if True converts the orphan member triggers to standard triggers
+         """
+         params = {'keepNonOrphans': str(keep_non_orphans).lower(), 'keepOrphans': str(keep_orphans).lower()}
+         self._delete(self._service_url(['triggers', 'groups', group_id], params=params))
+
     def create_group_member(self, member):
         data = self._serialize_object(member)
         return Trigger(self._post(self._service_url(['triggers', 'groups', 'members']), data))
